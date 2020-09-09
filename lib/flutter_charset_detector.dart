4a0e438dc10abbe5dfa26ff4e6faac0c8991cc7b
Aaron Madlon-Kay <aaron@madlon-kay.com>
2020-09-09 23:26:54 +0900

    Define autoDecode interface on Dart side

--- lib/flutter_charset_detector.dart
+++ lib/flutter_charset_detector.dart
@@ -1,14 +1,26 @@
-
 import 'dart:async';
+import 'dart:typed_data';
 
 import 'package:flutter/services.dart';
 
-class FlutterCharsetDetector {
+class CharsetDetector {
   static const MethodChannel _channel =
       const MethodChannel('flutter_charset_detector');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  static Future<DecodingResult> autoDecode(Uint8List bytes) async {
+    final result =
+        await _channel.invokeMethod<Map>('autoDecode', {'data': bytes});
+    return DecodingResult.fromJson(result.cast<String, dynamic>());
   }
+}
+
+class DecodingResult {
+  DecodingResult.fromJson(Map<String, dynamic> json)
+      : this._(json['encoding'] as String, json['string'] as String);
+
+  DecodingResult._(this.encoding, this.string)
+      : assert(encoding != null),
+        assert(string != null);
+  final String encoding;
+  final String string;
 }
