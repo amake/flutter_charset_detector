0ec2dec7725185a3b1fe4ce74b50cdcc2cc17c90
Aaron Madlon-Kay <aaron@madlon-kay.com>
2020-09-11 16:20:17 +0900

    Add doc strings to public API

--- lib/flutter_charset_detector.dart
+++ lib/flutter_charset_detector.dart
@@ -7,6 +7,7 @@ class CharsetDetector {
   static const MethodChannel _channel =
       MethodChannel('flutter_charset_detector');
 
+  /// Automatically detect the charset of [bytes] and decode to a string.
   static Future<DecodingResult> autoDecode(Uint8List bytes) async {
     final result =
         await _channel.invokeMethod<Map>('autoDecode', {'data': bytes});
@@ -14,6 +15,7 @@ class CharsetDetector {
   }
 }
 
+/// The result of decoding bytes to a string
 class DecodingResult {
   DecodingResult._fromJson(Map<String, dynamic> json)
       : this._(
@@ -24,6 +26,10 @@ class DecodingResult {
   DecodingResult._(this.charset, this.string)
       : assert(charset != null),
         assert(string != null);
+
+  /// The original charset of [string]
   final String charset;
+
+  /// The decoded string
   final String string;
 }
