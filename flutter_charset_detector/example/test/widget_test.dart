36ca468aa2f8d8f40239a142373feb13ecbe11cf
Aaron Madlon-Kay <aaron@madlon-kay.com>
2022-12-11 16:35:08 +0900

    Fix example test

--- flutter_charset_detector/example/test/widget_test.dart
+++ flutter_charset_detector/example/test/widget_test.dart
@@ -1,15 +1,20 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_charset_detector_example/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('Launch app', (tester) async {
-    await tester.pumpWidget(const MyApp());
-
-    expect(
-      find.byWidgetPredicate((widget) =>
-          widget is Text && widget.data!.startsWith('Default Charset')),
-      findsOneWidget,
-    );
+    await runZonedGuarded(() async {
+      await tester.pumpWidget(const MyApp());
+      expect(
+        find.byWidgetPredicate((widget) =>
+            widget is Text && widget.data!.startsWith('Default Charset')),
+        findsOneWidget,
+      );
+    }, (error, stack) {
+      expectSync(error, isA<FormatException>());
+    });
   });
 }
