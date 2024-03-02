99b769f4f96c02bb69a628c366ba0f2940f34398
Aaron Madlon-Kay <aaron@madlon-kay.com>
2024-03-02 21:05:56 +0900

    Fix registration of web plugin

--- flutter_charset_detector_web/lib/flutter_charset_detector_web.dart
+++ flutter_charset_detector_web/lib/flutter_charset_detector_web.dart
@@ -3,6 +3,7 @@ import 'package:flutter_charset_detector_platform_interface/decoding_result.dart
 import 'package:flutter_charset_detector_platform_interface/flutter_charset_detector_platform_interface.dart';
 import 'package:flutter_charset_detector_web/js_charset_detector.dart'
     as js_charset;
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 class CharsetDetectorWeb extends CharsetDetectorPlatform {
   CharsetDetectorWeb() {
@@ -12,7 +13,7 @@ class CharsetDetectorWeb extends CharsetDetectorPlatform {
   }
 
   /// Registers this class as the default instance of [CharsetDetectorPlatform]
-  static void registerWith() =>
+  static void registerWith(Registrar registrar) =>
       CharsetDetectorPlatform.instance = CharsetDetectorWeb();
 
   /// Automatically detect the charset of [bytes] and decode to a string.
