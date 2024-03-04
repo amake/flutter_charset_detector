a354d913ee816b5d370b3e366705c9e315d3c018
Aaron Madlon-Kay <aaron@madlon-kay.com>
2024-03-05 08:02:12 +0900

    Add macOS to example app

---
+++ flutter_charset_detector/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_charset_detector_darwin
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterCharsetDetectorPlugin.register(with: registry.registrar(forPlugin: "FlutterCharsetDetectorPlugin"))
+}
